8e334a6e9416f722f2f7f28bbc02e2b4b30bdd35
freakkk <freakkk@outlook.com>
2014-10-18 15:57:09 +0800

    hui

---
+++ InterpersonalRelationsPlatform/DataInterface.m
@@ -0,0 +1,23 @@
+//
+//  DataInterface.m
+//  InterpersonalRelationsPlatform
+//
+//  Created by Ibokan on 14-10-18.
+//  Copyright (c) 2014年 Ibokan. All rights reserved.
+//
+
+#import "DataInterface.h"
+
+#define WiressSDKDemoAppKey     @"801213517"
+#define WiressSDKDemoAppSecret  @"9819935c0ad171df934d0ffb340a3c2d"
+#define REDIRECTURI             @"http://www.ying7wang7.com"
+
+@implementation DataInterface
+@synthesize wbapi;
+
++(void)login
+{
+    
+}
+
+@end
---
+++ InterpersonalRelationsPlatform/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  InterpersonalRelationsPlatform
+//
+//  Created by Ibokan on 14-10-18.
+//  Copyright (c) 2014年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
