af92449c1d12ab2a1020b63f88554ca91a5ebcc7
IceSnow <xinyi_yangcd@sina.com>
2014-10-18 16:19:30 +0800

    Roster

---
+++ InterpersonalRelationsPlatform/HDLjjdjdjdjdjd.h
@@ -0,0 +1,13 @@
+//
+//  HDLjjdjdjdjdjd.h
+//  InterpersonalRelationsPlatform
+//
+//  Created by 3001 on 14/10/18.
+//  Copyright (c) 2014年 Ibokan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HDLjjdjdjdjdjd : NSObject
+
+@end
